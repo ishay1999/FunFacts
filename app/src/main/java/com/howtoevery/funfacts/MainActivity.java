52c6202b7417e53d64974737d5eca51cb3275adf
Ishay Frenkel <howtoevery.mail@gmail.com>
2015-06-09 22:56:46 +0300

    add colors, using function, no bugs and every random is different than the previous.

--- app/src/main/java/com/howtoevery/funfacts/MainActivity.java
+++ app/src/main/java/com/howtoevery/funfacts/MainActivity.java
@@ -14,58 +14,80 @@ import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
+    // Creating the random
+    Random rand = new Random();
 
+    TextView factLabel;
+    Button showFactButton;
+    ViewGroup backGround;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Creating the random
-        final Random rand = new Random();
+
+
+
 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView factLabel;
-        final Button showFactButton;
+        final String[] facts = {
+                "Ants stretch when they wake up in the morning.",
+                "Ostriches can run faster than horses.",
+                "Olympic gold medals are actually made mostly of silver.",
+                "You are born with 300 bones; by the time you are an adult you will have 206.",
+                "It takes about 8 minutes for light from the Sun to reach Earth.",
+                "Some bamboo plants can grow almost a meter in just one day.",
+                "The state of Florida is bigger than England.",
+                "Some penguins can leap 2-3 meters out of the water.",
+                "On average, it takes 66 days to form a new habit.",
+                "Mammoths still walked the earth when the Great Pyramid was being built." };
+
+        final int [] colors = {
+                R.color.orangish,
+                R.color.android_green,
+                R.color.ugly_sea,
+                R.color.turkiz,
+                R.color.shilshul,
+                R.color.nice_purple,
+                R.color.gray,
+                R.color.gay_pink,
+                R.color.bordo,
+                R.color.red
+        };
+
 
         factLabel = (TextView) findViewById(R.id.fact);
-        final ViewGroup backGround = (RelativeLayout) findViewById(R.id.mainBackground);
+        backGround = (RelativeLayout) findViewById(R.id.mainBackground);
         showFactButton = (Button) findViewById(R.id.factButton);
 
         View.OnClickListener myListener = new View.OnClickListener() {
 
-            int randomNumber;
             int currentState = 0;
+            int randomNumber;
 
             @Override
             public void onClick(View view) {
 
                 do {
-                    randomNumber = rand.nextInt(2); // randomNumber will be 0 or 1
-                }   while (currentState == randomNumber);
+                    randomNumber = rand.nextInt(facts.length);
+                } while (currentState == randomNumber);
+
+                    //    setFact(facts[randomFact], colors[randomColor]);
+//                factLabel.setText(facts[randomFact]);
+//                backGround.setBackgroundColor(getResources().getColor(colors[randomColor]));
+//                showFactButton.setTextColor(colors[randomColor]);
+
+                setFact(facts[randomNumber], colors[randomNumber]);
 
                 currentState = randomNumber;
-
-                switch (randomNumber) {
-                    case 0: { // in case it is 0, show fact number 1 with green color
-                        setFact(R.string.fact1, R.color.android_green);
-                    } break;
-
-                    case 1: { // in case it is 1, show fact number 2 with orange color
-                        setFact(R.string.fact2, R.color.orangish);
-
-                    } break;
-                }
-
-          //      factLabel.setText(String.valueOf(randomNumber)); // show the number generated with randomNumber, ONLY FOR DEBUG PURPOSES
-
             }
         };
 
         showFactButton.setOnClickListener(myListener);
     }
-    public void setFact(int factString, int color) {
+    public void setFact(String factString, int color) {
         factLabel.setText(factString);
         backGround.setBackgroundColor(getResources().getColor(color));
         showFactButton.setTextColor(getResources().getColor(color));
