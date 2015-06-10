27e17c2681a28687c155f1f09202609aac903377
Ishay Frenkel <howtoevery.mail@gmail.com>
2015-06-10 10:23:23 +0300

    add Toast message

--- app/src/main/java/com/howtoevery/funfacts/MainActivity.java
+++ app/src/main/java/com/howtoevery/funfacts/MainActivity.java
@@ -2,13 +2,12 @@ package com.howtoevery.funfacts;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -21,41 +20,38 @@ public class MainActivity extends ActionBarActivity {
     Button showFactButton;
     ViewGroup backGround;
 
+    String[] facts = {
+            "Ants stretch when they wake up in the morning.",
+            "Ostriches can run faster than horses.",
+            "Olympic gold medals are actually made mostly of silver.",
+            "You are born with 300 bones; by the time you are an adult you will have 206.",
+            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "Some bamboo plants can grow almost a meter in just one day.",
+            "The state of Florida is bigger than England.",
+            "Some penguins can leap 2-3 meters out of the water.",
+            "On average, it takes 66 days to form a new habit.",
+            "Mammoths still walked the earth when the Great Pyramid was being built."};
+
+    int[] colors = {
+            R.color.orangish,
+            R.color.android_green,
+            R.color.ugly_sea,
+            R.color.turkiz,
+            R.color.shilshul,
+            R.color.nice_purple,
+            R.color.gray,
+            R.color.gay_pink,
+            R.color.bordo,
+            R.color.red
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
-
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final String[] facts = {
-                "Ants stretch when they wake up in the morning.",
-                "Ostriches can run faster than horses.",
-                "Olympic gold medals are actually made mostly of silver.",
-                "You are born with 300 bones; by the time you are an adult you will have 206.",
-                "It takes about 8 minutes for light from the Sun to reach Earth.",
-                "Some bamboo plants can grow almost a meter in just one day.",
-                "The state of Florida is bigger than England.",
-                "Some penguins can leap 2-3 meters out of the water.",
-                "On average, it takes 66 days to form a new habit.",
-                "Mammoths still walked the earth when the Great Pyramid was being built." };
-
-        final int [] colors = {
-                R.color.orangish,
-                R.color.android_green,
-                R.color.ugly_sea,
-                R.color.turkiz,
-                R.color.shilshul,
-                R.color.nice_purple,
-                R.color.gray,
-                R.color.gay_pink,
-                R.color.bordo,
-                R.color.red
-        };
 
 
         factLabel = (TextView) findViewById(R.id.fact);
@@ -74,7 +70,7 @@ public class MainActivity extends ActionBarActivity {
                     randomNumber = rand.nextInt(facts.length);
                 } while (currentState == randomNumber);
 
-                    //    setFact(facts[randomFact], colors[randomColor]);
+                //    setFact(facts[randomFact], colors[randomColor]);
 //                factLabel.setText(facts[randomFact]);
 //                backGround.setBackgroundColor(getResources().getColor(colors[randomColor]));
 //                showFactButton.setTextColor(colors[randomColor]);
@@ -86,32 +82,22 @@ public class MainActivity extends ActionBarActivity {
         };
 
         showFactButton.setOnClickListener(myListener);
+
+//        String message = "Yay! bla bla";
+//        Toast welcomeToast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+//        welcomeToast.show();
+        
+        // or in a shorter way:
+        Toast.makeText(this, "Yay! bla bla", Toast.LENGTH_SHORT).show();
+
     }
+
     public void setFact(String factString, int color) {
         factLabel.setText(factString);
         backGround.setBackgroundColor(getResources().getColor(color));
         showFactButton.setTextColor(getResources().getColor(color));
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
+
+
